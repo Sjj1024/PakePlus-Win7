16735bc353b0739b225bf7e1f9e9d00381975ed6
1024小神 <15670339118@163.com>
2025-11-12 09:26:53 +0800

    Sure! Pl

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    websiteUrl: 'https://pakeplus.com/zh/',
+
+    windowWidth: 1200,
+    windowHeight: 800,
+    minWidth: 800,
+    minHeight: 600,
+    openDevTools: false,
+    appTitle: 'PakePlus',
+}
