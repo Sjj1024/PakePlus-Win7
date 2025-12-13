385a5e049c81c65e83bc8d5d9c43c53300844eb5
1024小神 <15670339118@163.com>
2025-12-13 11:42:39 +0800

    Remove config.js file to streamline configuration management and eliminate unused settings.

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    websiteUrl: 'render/index.html',
-    windowWidth: 1200,
-    windowHeight: 800,
-    minWidth: 800,
-    minHeight: 600,
-    openDevTools: false,
-    appTitle: 'PakePlus',
-}
